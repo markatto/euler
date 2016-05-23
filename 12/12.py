138e166d46dcb61622be55e76af471a4fbf55afc
Mark Merritt <mark@marktmerritt.com>
2016-05-23 18:59:39 -0400

    cleanup #12

--- 12/12.py
+++ 12/12.py
@@ -3,18 +3,16 @@ import itertools
 import collections
 import sys
 
-# generate triangle numbers (1+2+3+...)
+# You really want to run this with pypy
+
 def triangle_number_gen():
+    '''generate triangle numbers (1+2+3+...)'''
     n = 0
     x = 0
     while True:
         n += 1
         x += n
         yield x
-
-def count_divisors(x):
-    '''this is waay too slow, but it reminds me of haskell so I'm leaving it here'''
-    return sum((1 for i in range(1, x + 1) if x % i == 0))
 
 def sieve_below(x):
     # sieve[n] is the factor count of n
@@ -33,13 +31,13 @@ def sieve_below(x):
 
 
 if __name__ == "__main__":
-    n = 32
+    n = 4096
     while True:
         sieve = sieve_below(n)
         for number in sieve:
-            print("{}: {}".format(number, sieve[number]))
             if sieve[number] > 500:
-                print(number)
+                print("{}: {}".format(number, sieve[number]))
                 sys.exit(0)
         n = n * 2
+        print("expanding to size {}".format(n))
 
