0405e47c3c572b6fbb809396b61416128d9b7837
Mark Merritt <mark@marktmerritt.com>
2016-05-23 17:42:24 -0400

    remove some magic numbers

--- 8/8.py
+++ 8/8.py
@@ -1,14 +1,15 @@
 #! /usr/bin/env python3
 import functools
 
+n = 13
 with open('numbers_file') as f:
     numbers = f.read()
 
 numbers =[int(number) for number in numbers.strip()]
 
 highest = 0
-for i in range(len(numbers) - 12):
-    subset = numbers[i:i+13]
+for i in range(len(numbers) - (n - 1)):
+    subset = numbers[i:i+n]
     product = functools.reduce(lambda x,y: x*y, subset)
     if product > highest:
         highest = product
