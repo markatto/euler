4690c1341c78eb9572a889e33133c819f8911ce0
Mark Merritt <mark@marktmerritt.com>
2016-05-23 18:05:32 -0400

    make 9 a bit prettier

--- 9/9.py
+++ 9/9.py
@@ -4,7 +4,15 @@ from math import sqrt,floor
 from functools import reduce
 from operator import mul
 
-triplets = [x for x in combinations_with_replacement(range(1000), 3) if sum(x) == 1000 and (x[0]**2 + x[1]**2 == x[2]**2 or x[0]**2 + x[2]**2 == x[1]**2 or x[1]**2 + x[2]**2 == x[0]**2)]
+n = 1000
+
+def is_pythagorean_triplet(triplet):
+	'''a**2 + b**2 == c**2'''
+	t = sorted(triplet) # c needs to be the biggest one
+	return triplet[0]**2 + triplet[1]**2 == triplet[2]**2
+
+triplets = combinations_with_replacement(range(1, n), 3) 
+triplets = (t for t in triplets if sum(t) == n and is_pythagorean_triplet(t))
 
 print([reduce(mul, triplet) for triplet in triplets])
 
